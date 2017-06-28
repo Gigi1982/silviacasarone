630917b57351b012f842c34c988c88720a7f3936
Gigi1303 <basile.luigi@gmail.com>
2017-06-28 17:38:50 +0200

    single-progetti mod

--- wp-content/themes/silviacasarone-theme/single-progetti.php
+++ wp-content/themes/silviacasarone-theme/single-progetti.php
@@ -29,7 +29,7 @@
                         <?php endif; ?>
                         <?php if(get_field('progetti_struttura')): ?>
                         <div class="single-pages-metadata-block">
-                            <h5 class="single-pages-metadata-title">Struttura</h5>
+                            <h5 class="single-pages-metadata-title">Durata</h5>
                             <span class="single-pages-metadata-content green"><?php the_field('progetti_struttura'); ?></span>
                         </div>
                         <?php endif; ?> 
@@ -39,6 +39,12 @@
                             <span class="single-pages-metadata-content green"><?php the_field('progetti_partecipanti'); ?></span>
                         </div>
                         <?php endif; ?> 
+                        <?php if(get_field('progetti_partecipanti')): ?>
+                        <div class="single-pages-metadata-block">
+                            <h5 class="single-pages-metadata-title">In collaborazione con</h5>
+                            <span class="single-pages-metadata-content green"><?php the_field('progetti_partnership'); ?></span>
+                        </div>
+                        <?php endif; ?> 
                     </div>
                     <div class="col-md-8">
                         <?php if(get_field('progetto_testo')): ?>
@@ -46,38 +52,32 @@
                         <?php endif; ?> 
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
-    <section class="single-project-map">
-        <?php 
-
-        $location = get_field('mappa_progetto');
-
-        if( !empty($location) ):
-        ?>
-        <div class="acf-map">
-            <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-        </div>
-        <?php endif; ?>
-    </section>
-    <div class="container">
-        <section class="single-pages-body">
-            <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="single-pages-metadata-block">
-                            <h3 class="single-pages-left-title">Finalità</h3>
-                        </div>
+                        <section class="single-project-map">
+                            <?php 
+
+                            $location = get_field('mappa_progetto');
+
+                            if( !empty($location) ):
+                            ?>
+                            <div class="acf-map">
+                                <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+                            </div>
+                            <?php endif; ?>
+                        </section>
                     </div>
                     <div class="col-md-8">
                         <?php if(get_field('progetto_finalita')): ?>
-                        <p class=""><?php the_field('progetto_finalita'); ?></p>
+                        <div class=""><?php the_field('progetto_finalita'); ?></div>
                         <?php endif; ?> 
                     </div>
                 </div>
             </div>
         </section>
+    </div>
+    
+    <div class="container">
         <div class="sc-payoff single-pages-footer no-bg">
             <div class="container-fluid"><span class="black">PROPOSTO DA </span> <span class="white"><?php the_field('progetto_associazione'); ?></span><span class="black">CON IL SOSTEGNO DI </span><span class="white"><?php the_field('progetto_sostegno'); ?></span></div>
         </div>
